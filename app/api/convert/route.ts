489d87f01936f21ee7cbd515982ae4ca0a890b6d
Hardik-Kumar0005 <hardikkumarpro0005@gmail.com>
2025-12-16 15:30:54 +0530

    set api routes to use edge runtime

--- app/api/convert/route.ts
+++ app/api/convert/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import sharp from 'sharp';
 
+export const runtime = "edge";
+
 export async function POST(req: NextRequest) {
     try {
         const formData = await req.formData();
