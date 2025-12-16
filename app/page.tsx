f4138c5a2c2afbad545b9abbfd60e57dc82ddd83
Hardik-Kumar0005 <hardikkumarpro0005@gmail.com>
2025-12-16 14:42:51 +0530

    add repo link

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ export default function Home() {
   return (
     <main className="min-h-screen py-24 px-4 flex flex-col items-center justify-center relative overflow-hidden">
 
+      <div className="absolute top-4 right-4">
+        <a
+          href="https://github.com/hardik-xi11/free-image"
+          className='text-yellow-500 hover:text-yellow-300 transition-colors'
+        >
+          ⭐ on GitHub
+        </a>
+      </div>
+
       {/* Background Glows */}
       <div className="absolute top-0 right-0 w-[500px] h-[500px] bg-blue-500/10 blur-[120px] rounded-full pointer-events-none" />
       <div className="absolute bottom-0 left-0 w-[500px] h-[500px] bg-red-500/10 blur-[120px] rounded-full pointer-events-none" />
